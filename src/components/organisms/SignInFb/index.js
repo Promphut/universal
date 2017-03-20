b3e08a2dddb4fd977a3edecf8f8377fa01b52edc
ochawin <ochawin@likemeinc.com>
2017-03-20 14:38:05 +0700

    add hasroles middleware for routes + fix some login bug

--- src/components/organisms/SignInFb/index.js
+++ src/components/organisms/SignInFb/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import styled from 'styled-components'
 import Avatar from 'material-ui/Avatar'
 import RaisedButton from 'material-ui/RaisedButton';
@@ -83,7 +83,7 @@ const SignInFb = React.createClass({
     <Box>
       <Head>Sign In</Head>
       <div style={styles.btnCon}>
-        <a href={config.BACKURL+'/auth/facebook?publisher='+config.PID}>
+        <a href={config.BACKURL+'/auth/facebook?publisher='+config.PID+'&nextpathname='+encodeURIComponent(this.props.nextPathname)}>
           <RaisedButton
             label=" Sign In with facebook"
             labelPosition="after"
@@ -104,6 +104,10 @@ const SignInFb = React.createClass({
   )
   },
 })
+
+SignInFb.propTypes = {
+  nextPathname: PropTypes.string.isRequired
+}
 
 
 export default SignInFb
