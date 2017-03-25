ece600f589c871cdfce0368c0e9a282247cbd86f
ochawin <ochawin@likemeinc.com>
2017-03-25 23:49:31 +0700

    change production config

--- config/index.js
+++ config/index.js
@@ -18,20 +18,24 @@ const config = {
 
 		}
 	},
+
 	production : {
 		PROFILE:'/tmp/avatar.png',
 		TWT:'https://twitter.com/intent/tweet',
 		PID:'11',
 		MAINCOLOR:'#00B2B4',
-		DOMAIN: 		'localhost',
-		FRONTURL: 		'http://localhost:3000',
-		BACKURL: 		'http://localhost:4000',
+		DOMAIN: 		'nextempire',
+		FRONTURL: 		'http://nextempire.co',
+		BACKURL: 		'http://api.nextempire.co',
 		FEED_LIMIT: 15,
 		ROLES: {
 			MEMBER: 0, 
 			EDITOR: 1, 
 			WRITER: 2, 
 			ADMIN: 3
+		},
+		THEME:{ 
+
 		}
 	}
 }
