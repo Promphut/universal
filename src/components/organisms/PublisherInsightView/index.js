2f7f551832ab498e023ca80a311211fb528d3277
Ratthapon <wittayasutt@gmail.com>
2017-04-07 19:48:29 +0700

    Table

--- src/components/organisms/PublisherInsightView/index.js
+++ src/components/organisms/PublisherInsightView/index.js
@@ -7,9 +7,28 @@ import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow,
 const Container = styled.div`
 `
 
+const Bold = styled.div`
+	font-weight: bold;
+`
+
 const styles = {
-  tableText: {
-		fontSize: '16px'
+  tableTextHeader: {
+		fontSize: '16px',
+		fontWeight: 'bold',
+		color: '#001738',
+		textAligh: 'center'
+  },
+  tableTextBodyName: {
+		fontSize: '16px',
+		textAligh: 'center',
+		whiteSpace: 'initial',
+		padding: '12px 24px',
+		lineHeight: '25px',
+		width: '30%'
+  },
+  tableTextBody: {
+		fontSize: '16px',
+		textAligh: 'center'
   }
 }
 
@@ -21,36 +40,42 @@ const PublisherInsightView = React.createClass({
 
   render() {
     return (
-      <Container className="sans-font">
+      <Container>
 				<Table>
 					<TableHeader
             displaySelectAll={false}
             adjustForCheckbox={false}
 					>
-						<TableRow style={{fontSize: '22px'}}>
-							<TableHeaderColumn style={styles.tableText}></TableHeaderColumn>
-							<TableHeaderColumn style={styles.tableText}>Today</TableHeaderColumn>
-							<TableHeaderColumn style={styles.tableText}>Previous Week</TableHeaderColumn>
-							<TableHeaderColumn style={styles.tableText}>Previous 2 Weeks</TableHeaderColumn>
-							<TableHeaderColumn style={styles.tableText}>Overall</TableHeaderColumn>
+						<TableRow className="sans-font">
+							<TableHeaderColumn style={styles.tableTextBodyName}></TableHeaderColumn>
+							<TableHeaderColumn style={styles.tableTextHeader}>Today</TableHeaderColumn>
+							<TableHeaderColumn style={styles.tableTextHeader}>Previous Week</TableHeaderColumn>
+							<TableHeaderColumn style={styles.tableTextHeader}>Previous 2 Weeks</TableHeaderColumn>
+							<TableHeaderColumn style={styles.tableTextHeader}>Overall</TableHeaderColumn>
 						</TableRow>
 					</TableHeader>
 					<TableBody
 						displayRowCheckbox={false}
 					>
-						<TableRow>
-							<TableRowColumn style={styles.tableText}>1</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>John Smith</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Employed</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Employed</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Employed</TableRowColumn>
+						<TableRow className="sans-font">
+							<TableRowColumn style={styles.tableTextBodyName}>
+								<Bold>1. A New Drought Simulation Has Concluded With Frightening Results</Bold>
+								Dr.Nut
+							</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
 						</TableRow>
-						<TableRow>
-							<TableRowColumn style={styles.tableText}>2</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Randal White</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Unemployed</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Randal White</TableRowColumn>
-							<TableRowColumn style={styles.tableText}>Unemployed</TableRowColumn>
+						<TableRow className="sans-font">
+							<TableRowColumn style={styles.tableTextBodyName}>
+								<Bold>1. A New Drought Simulation Has Concluded With Frightening Results</Bold>
+								Dr.Nut
+							</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
+							<TableRowColumn style={styles.tableTextBody}>13,123</TableRowColumn>
 						</TableRow>
 					</TableBody>
 				</Table>
