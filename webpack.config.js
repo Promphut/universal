3f6ca6f80fcaa25d8a485c188b7cc16150e3b713
ochawin <ochawin@likemeinc.com>
2017-07-17 15:46:26 +0700

    unminify webpack for staging

--- webpack.config.js
+++ webpack.config.js
@@ -214,7 +214,8 @@ const client = createConfig([
     ])
   ]) : env(process.env.NODE_ENV, [
     splitVendor({ exclude: [/lodash/, /offline-plugin\/runtime\.js/] }),
-    addPlugins([
+    
+    process.env.NODE_ENV==='production' && addPlugins([
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
