1d7b7b779195906ab1784c3a794934023f5a2f78
Promphut <phiphut.ch@gmail.com>
2017-08-08 15:17:09 +0700

    template

--- shared/components/pages/Template/Template.js
+++ shared/components/pages/Template/Template.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { TopBarWithNavigation,Footer } from 'components'
+import { TopBarWithNavigation,Footer } from '../../../components'
 // Main Controller Style Goes Here!
 const Wrapper = styled.div `
 `
