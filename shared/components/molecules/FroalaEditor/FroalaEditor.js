58cae0c3a1026ad96dfd3b3417732db4bf666b03
wittaysutt <wittaysutt@gmail.com>
2017-09-27 16:55:48 +0700

    froala

--- shared/components/molecules/FroalaEditor/FroalaEditor.js
+++ shared/components/molecules/FroalaEditor/FroalaEditor.js
@@ -20,129 +20,130 @@ class FroalaEditor extends React.Component {
 	constructor(props) {
 		super(props)
 
-		this.state = {}
+		this.state = {
+			froalaConfig = {
+				charCounterCount: false,
+				imageEditButtons: [
+					'imageAlignCustom',
+					'imageAlt',
+					'|',
+					'imageLink',
+					'linkOpen',
+					'linkEdit',
+					'linkRemove'
+				],
+				imageInsertButtons: ['imageBack', '|', 'imageUpload', 'imageByURL'],
+				imageResize: false,
+				imageDefaultAlign: 'center',
+				inlineStyles: {
+					Caption: "font-family: 'PT Sans', 'cs_prajad'; font-size: 16px; color: #8e8e8e; font-weight: normal;",
+					Source: "font-family:'PT Sans', 'cs_prajad'; font-size: 16px; color: #8e8e8e; font-weight: normal;"
+					// 'Image source': "font-family: 'PT Sans', 'cs_prajad'; font-size: 14px; color: #CCC; font-style: italic; line-height: .1;"
+				},
+				linkEditButtons: ['linkOpen', 'linkEdit', 'linkRemove'],
+				linkInsertButtons: ['linkBack'],
+				paragraphFormat: {
+					H2: 'Head',
+					H3: 'Subhead',
+					N: 'Normal'
+				},
+				paragraphMultipleStyles: false,
+				paragraphStyles: {
+					'fr-text-caption': 'Caption',
+					'fr-text-source': 'Source'
+				},
+				placeholderText: 'Write ...',
+				quickInsertButtons: ['image', 'table', 'hr'],
+				tableEditButtons: [
+					'tableHeader',
+					'tableRemove',
+					'|',
+					'tableRows',
+					'tableColumns',
+					'-',
+					'tableCells',
+					'tableCellBackground',
+					'tableCellVerticalAlign',
+					'tableCellHorizontalAlign'
+				],
+				tabSpaces: 4,
+				toolbarButtons: [
+					'bold',
+					'italic',
+					'underline',
+					'|',
+					'align',
+					'formatOL',
+					'formatUL',
+					'insertHR',
+					'|',
+					'paragraphFormat',
+					'paragraphStyle',
+					'quote',
+					'|',
+					'insertLink',
+					'insertImage',
+					'insertVideo',
+					'insertTable'
+				],
+				// toolbarInline: true,
+				// toolbarVisibleWithoutSelection: true,
+				videoDefaultWidth: 730,
+				videoEditButtons: [],
+				videoInsertButtons: ['videoBack', '|', 'videoByURL', 'videoEmbed'],
+				videoResize: true,
+				imageUploadParam: 'image',
+				imageMaxSize: 1024 * 1024 * 10,
+				events: {
+					'froalaEditor.paste.beforeCleanup': function(e, editor, clipboard_html) {
+						var $html = $('<div>' + clipboard_html + '</div>')
+						var $span = $html.find('span')
+						$span.each(function(i, field) {
+							var style = field.style
+							var s = style['font-weight'] == 700
+							var ul = style['text-decoration'] == 'underline'
+							var fs = style['font-style'] == 'italic'
+							$(field).html(
+								`${s ? '<strong>' : ''}${fs ? '<em>' : ''}${ul ? '<u>' : ''}${field.innerHTML}${ul ? '</u>' : ''}${fs ? '</em>' : ''}${s ? '</strong>' : ''}`
+							)
+						})
+						var newHTML = $html.html()
+						// console.log(newHTML)
+						return newHTML
+					}
+				}
+			},
+			froalaConfigHighlight = {
+				charCounterCount: false,
+				linkEditButtons: ['linkOpen', 'linkEdit', 'linkRemove'],
+				linkInsertButtons: ['linkBack'],
+				placeholderText: 'Write Highlight ...',
+				quickInsertButtons: false,
+				tabSpaces: 4,
+				toolbarButtons: [
+					'bold',
+					'italic',
+					'underline',
+					'|',
+					'formatOL',
+					'formatUL',
+					'|',
+					'insertLink'
+				],
+				toolbarInline: true,
+				toolbarVisibleWithoutSelection: true
+			}
+		}
 	}
 
 	componentWillReceiveProps = nextProps => {
 		if (this.props.sid !== nextProps.sid) {
 			this.froalaConfig.imageUploadURL = this.props.imgURL
 				? this.props.imgURL
-				: `${BACKURL}/stories/${this.props.sid}/image`
+				: `${BACKURL}/stories/${nextProps.sid}/image`
 		}
 	}
 
-	froalaConfig = {
-		charCounterCount: false,
-		imageEditButtons: [
-			'imageAlignCustom',
-			'imageAlt',
-			'|',
-			'imageLink',
-			'linkOpen',
-			'linkEdit',
-			'linkRemove'
-		],
-		imageInsertButtons: ['imageBack', '|', 'imageUpload', 'imageByURL'],
-		imageResize: false,
-		imageDefaultAlign: 'center',
-		inlineStyles: {
-			Caption: "font-family: 'PT Sans', 'cs_prajad'; font-size: 16px; color: #8e8e8e; font-weight: normal;",
-			Source: "font-family:'PT Sans', 'cs_prajad'; font-size: 16px; color: #8e8e8e; font-weight: normal;"
-			// 'Image source': "font-family: 'PT Sans', 'cs_prajad'; font-size: 14px; color: #CCC; font-style: italic; line-height: .1;"
-		},
-		linkEditButtons: ['linkOpen', 'linkEdit', 'linkRemove'],
-		linkInsertButtons: ['linkBack'],
-		paragraphFormat: {
-			H2: 'Head',
-			H3: 'Subhead',
-			N: 'Normal'
-		},
-		paragraphMultipleStyles: false,
-		paragraphStyles: {
-			'fr-text-caption': 'Caption',
-			'fr-text-source': 'Source'
-		},
-		placeholderText: 'Write ...',
-		quickInsertButtons: ['image', 'table', 'hr'],
-		tableEditButtons: [
-			'tableHeader',
-			'tableRemove',
-			'|',
-			'tableRows',
-			'tableColumns',
-			'-',
-			'tableCells',
-			'tableCellBackground',
-			'tableCellVerticalAlign',
-			'tableCellHorizontalAlign'
-		],
-		tabSpaces: 4,
-		toolbarButtons: [
-			'bold',
-			'italic',
-			'underline',
-			'|',
-			'align',
-			'formatOL',
-			'formatUL',
-			'insertHR',
-			'|',
-			'paragraphFormat',
-			'paragraphStyle',
-			'quote',
-			'|',
-			'insertLink',
-			'insertImage',
-			'insertVideo',
-			'insertTable'
-		],
-		// toolbarInline: true,
-		// toolbarVisibleWithoutSelection: true,
-		videoDefaultWidth: 730,
-		videoEditButtons: [],
-		videoInsertButtons: ['videoBack', '|', 'videoByURL', 'videoEmbed'],
-		videoResize: true,
-		imageUploadParam: 'image',
-		imageMaxSize: 1024 * 1024 * 10,
-		events: {
-			'froalaEditor.paste.beforeCleanup': function(e, editor, clipboard_html) {
-				var $html = $('<div>' + clipboard_html + '</div>')
-				var $span = $html.find('span')
-				$span.each(function(i, field) {
-					var style = field.style
-					var s = style['font-weight'] == 700
-					var ul = style['text-decoration'] == 'underline'
-					var fs = style['font-style'] == 'italic'
-					$(field).html(
-						`${s ? '<strong>' : ''}${fs ? '<em>' : ''}${ul ? '<u>' : ''}${field.innerHTML}${ul ? '</u>' : ''}${fs ? '</em>' : ''}${s ? '</strong>' : ''}`
-					)
-				})
-				var newHTML = $html.html()
-				// console.log(newHTML)
-				return newHTML
-			}
-		}
-	}
-	froalaConfigHighlight = {
-		charCounterCount: false,
-		linkEditButtons: ['linkOpen', 'linkEdit', 'linkRemove'],
-		linkInsertButtons: ['linkBack'],
-		placeholderText: 'Write Highlight ...',
-		quickInsertButtons: false,
-		tabSpaces: 4,
-		toolbarButtons: [
-			'bold',
-			'italic',
-			'underline',
-			'|',
-			'formatOL',
-			'formatUL',
-			'|',
-			'insertLink'
-		],
-		toolbarInline: true,
-		toolbarVisibleWithoutSelection: true
-	}
 
 	render() {
 		var newConfig = this.froalaConfig
